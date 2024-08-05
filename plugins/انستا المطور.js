1ce5176a4e5cee6b4a92a51c5b0ea1357c1a6b19
Ahmedaligue <158603161+Ahmedaligue@users.noreply.github.com>
2024-08-05 20:43:12 +0100

    Update انستا المطور.js

--- plugins/انستا المطور.js
+++ plugins/انستا المطور.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
 await conn.sendMessage(m.chat, { react: { text: '🦦', key: m.key } })
-m.reply(`*المطـور ♕𝙎𝙃𝙄𝙆𝘼🐥♕:*\n
-*『 https://instagram.com/shika17sm... 』*
+m.reply(`*المطـور ♕SENKU DEVLOPER 🐥♕:*\n
+*『 https://www.instagram.com/irin_yeger1?igsh=OGQ5ZDc2ODk2ZA==... 』*
 *اعمــل فولـو ياحـب رونالـدو بينـافسني في عدد الفلـورز🦦* `)
 }
 handler.help = ['انستا-المطور']
